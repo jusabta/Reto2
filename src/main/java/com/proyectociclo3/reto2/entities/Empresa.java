617686b4503164dd381bb91ed9bb752bd10f47c9
Jorge Silva <gerencia@imaginecons.com>
2022-09-22 20:41:40 -0500

    Inicio del Proyecto

---
+++ src/main/java/com/proyectociclo3/reto2/entities/Empresa.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyectociclo3.reto2.entities;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author LUNEL
+ */
+public class Empresa {
+    private long id;
+    private String nombre;
+    private String nit;
+    private String telefono;
+    private String direccion;
+    private LocalDate fechacreacion;
+    private LocalDate fechaactualizacion;
+
+    public Empresa() {
+    }
+
+    public Empresa(long id, String nombre, String nit, String telefono, String direccion, LocalDate fechacreacion, LocalDate fechaactualizacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.nit = nit;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.fechacreacion = fechacreacion;
+        this.fechaactualizacion = fechaactualizacion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public LocalDate getFechacreacion() {
+        return fechacreacion;
+    }
+
+    public void setFechacreacion(LocalDate fechacreacion) {
+        this.fechacreacion = fechacreacion;
+    }
+
+    public LocalDate getFechaactualizacion() {
+        return fechaactualizacion;
+    }
+
+    public void setFechaactualizacion(LocalDate fechaactualizacion) {
+        this.fechaactualizacion = fechaactualizacion;
+    }
+    
+    
+    
+}
